23a92be9d5162e912317e3c36d0d50b946dd3f9a
Giliade187 <giliade187@gmail.com>
2025-08-31 17:17:22 -0300

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Protocolo-casamento-dos-Sonhos/' : '/',
+  base: mode === 'production' ? '/casamentodossonhos/' : '/',
   server: {
     host: "::",
     port: 8080,
